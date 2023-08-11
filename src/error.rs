3703e5906827e276ff0a640de14b2bb1ddfcc8c1
Morgan Metz <mlmi4@k12albemarle.org>
2023-08-11 12:18:44 -0400

    minor changes to more simple version

--- src/error.rs
+++ src/error.rs
@@ -1,24 +1,13 @@
 use cosmwasm_std::StdError;
-use cw_ownable::OwnershipError;
 use thiserror::Error;
 
+/// Custom errors for this contract, add additional errors here.
 #[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
-    #[error(transparent)]
+    #[error("{0}")]
     Std(#[from] StdError),
 
-    #[error(transparent)]
-    Ownership(#[from] OwnershipError),
-
-    #[error(transparent)]
-    Version(#[from] cw2::VersionError),
-
-    #[error("token_id already claimed")]
-    Claimed {},
-
-    #[error("Cannot set approval that is already expired")]
-    Expired {},
-
-    #[error("Approval not found for: {spender}")]
-    ApprovalNotFound { spender: String },
+    /// This inherits from cw721-base::ContractError to handle the base contract errors
+    #[error("{0}")]
+    Cw721Error(#[from] cw721_base::ContractError),
 }
