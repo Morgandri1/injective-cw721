7ad0ef7f9297177ca8c4f47a760306989dcfb6ca
Morgan Metz <mlmi4@k12albemarle.org>
2023-08-16 18:13:15 -0400

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
-yes 12345678 | injectived tx wasm store injective721.wasm --from=genesis --chain-id="injective-1" --yes --gas-prices=500000000inj --gas=20000000
+yes 12345678 | injectived tx wasm store injective721.wasm --node="https://testnet.tm.injective.network/" --from=genesis --chain-id="injective-1" --yes --gas-prices=500000000inj --gas=20000000
 injectived query tx 4CFB63A47570C4CFBE8E669273B26BEF6EAFF922C07480CA42180C52219CE784
