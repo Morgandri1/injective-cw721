7c0e1a3ffb1e533462d50b96d15273da6697139c
Morgan Metz <mlmi4@k12albemarle.org>
2023-08-16 18:17:51 -0400

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 # inside the "injective-core-v1.10.1" container
 yes 12345678 | injectived tx wasm store injective721.wasm \
---from=inj1egav7klxhfd5cmrr7klwewyl56khwg4gmfue6a \
+--from=inj1wjsdqxghx9998zc303m5wtadwrz5y8946js5c9 \
 --chain-id="injective-888" \
 --yes --gas-prices=500000000inj --gas=20000000 \
 --node=https://k8s.testnet.tm.injective.network:443
