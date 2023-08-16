4fdc7a2d826710555d40799f3b2204c1fd389d5a
Morgan Metz <mlmi4@k12albemarle.org>
2023-08-16 18:15:21 -0400

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@ yes 12345678 | injectived tx wasm store /var/artifacts/cw20_base.wasm \
 --from=$(echo $INJ_ADDRESS) \
 --chain-id="injective-888" \
 --yes --gas-prices=500000000inj --gas=20000000 \
---node=https://k8s.testnet.tm.injective.network:443injectived 
+--node=https://k8s.testnet.tm.injective.network:443
